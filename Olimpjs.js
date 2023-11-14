8d66c6e4f2f92dbc1d6b648bff8309a062406746
endiepq <92216179+endiepq@users.noreply.github.com>
2023-11-15 02:27:50 +0300

    upd

---
+++ Olimpjs.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let slides = document.querySelectorAll(".slide");
+    let indicators = document.querySelector(".indicators");
+    let currentSlide = 0;
+
+    function showSlide(index) {
+        slides[currentSlide].classList.remove('active');
+        slides[index].classList.add('active');
+        updateActiveIndicator(index);
+        currentSlide = index;
+    }
+
+    function createIndicators() {
+        for (let i = 0; i < slides.length; i++) {
+            let dot = document.createElement("span");
+            dot.classList.add("dot");
+            dot.addEventListener("click", () => showSlide(i));
+            indicators.appendChild(dot);
+        }
+    }
+
+    function updateActiveIndicator(index) {
+        let dots = indicators.querySelectorAll(".dot");
+        dots.forEach(dot => dot.classList.remove('active'));
+        dots[index].classList.add('active');
+    }
+
+    createIndicators();
+    showSlide(0); // Показать первый слайд
+
+    // Если нужно автоматическое переключение
+    setInterval(() => {
+        let nextSlide = (currentSlide + 1) % slides.length;
+        showSlide(nextSlide);
+    }, 30000); // Переключать каждые 30 секунды
+});

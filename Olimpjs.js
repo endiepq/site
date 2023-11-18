9d202bb248d7f9de89a8fd5d4e216cc15777ce92
endiepq <92216179+endiepq@users.noreply.github.com>
2023-11-18 14:54:32 +0300

    upd

--- Olimpjs.js
+++ Olimpjs.js
@@ -4,11 +4,13 @@ document.addEventListener("DOMContentLoaded", () => {
     let currentSlide = 0;
     const navToggle = document.querySelector('.nav-toggle');
     const nav = document.querySelector('.nav');
-  
+
+    // Обработчик для кнопки меню
     navToggle.addEventListener('click', () => {
-      nav.classList.toggle('nav-visible');
-    })
-    
+        nav.classList.toggle('nav-visible');
+    });
+
+    // Функция для показа слайда
     function showSlide(index) {
         slides[currentSlide].classList.remove('active');
         slides[index].classList.add('active');
@@ -16,6 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
         currentSlide = index;
     }
 
+    // Создание индикаторов
     function createIndicators() {
         for (let i = 0; i < slides.length; i++) {
             let dot = document.createElement("span");
@@ -25,18 +28,46 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    // Обновление активного индикатора
     function updateActiveIndicator(index) {
         let dots = indicators.querySelectorAll(".dot");
         dots.forEach(dot => dot.classList.remove('active'));
         dots[index].classList.add('active');
     }
 
+    // Создание индикаторов и показ первого слайда
     createIndicators();
-    showSlide(0); // Показать первый слайд
+    showSlide(0);
 
-    // Если нужно автоматическое переключение
+    // Автоматическое переключение слайдов
     setInterval(() => {
         let nextSlide = (currentSlide + 1) % slides.length;
         showSlide(nextSlide);
     }, 30000); // Переключать каждые 30 секунды
+
+    // Выделение активного элемента в навбаре
+    var navItems = document.querySelectorAll('.nav a');
+
+    function updateActiveNavItem() {
+        var currentHash = window.location.hash;
+
+        navItems.forEach(function(item) {
+            item.classList.remove('active');
+            if (item.getAttribute('href') === currentHash) {
+                item.classList.add('active');
+            }
+        });
+    }
+
+    updateActiveNavItem();
+    window.addEventListener('hashchange', updateActiveNavItem);
+
+    // Обработчики для ссылок навигации
+    navItems.forEach(link => {
+        link.addEventListener('click', () => {
+            if (nav.classList.contains('nav-visible')) {
+                nav.classList.remove('nav-visible');
+            }
+        });
+    });
 });
